b47b2dc1690551da0b382a1f19e9e9a974ca59ac
katudd <katrin@uddstromer.se>
2018-01-10 00:12:19 +0100

    reinitialized files

---
+++ script.js
@@ -0,0 +1,13 @@
+// A function that adds and remove the class "active" on the section you click on.
+function toggle() {
+  this.classList.toggle("active")
+}
+
+// Selects and HTML element, and calls a function which will be executed when the element is clicked.
+document.getElementById("section1").onclick = toggle
+
+document.getElementById("section2").onclick = toggle
+
+document.getElementById("section3").onclick = toggle
+
+document.getElementById("section4").onclick = toggle
